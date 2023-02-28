f74840a8c0a3a2912d6f2c80c3e81303202f109f
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-03-01 00:05:13 +0530

    storing in LS, and fetching rest data from DB.

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,14 +1,21 @@
 const Chat = require("../models/chats");
 const User = require("../models/user");
+const { Op } = require("sequelize");
 
 exports.getUserName = async (req, res) => {
+  var messageCount = parseInt(req.query.messageCount);
+  console.log(messageCount);
   var attributes = ["messages"];
   const message = await Chat.findAll({
+    where: {
+      id: { [Op.lt]: messageCount },
+    },
     include: {
       model: User,
       attributes: ["name"],
     },
     attributes: attributes,
+    // order: [['createdAt', 'DESC']]
   });
 
   res.status(200).json({ messages: message });
@@ -17,10 +24,18 @@ exports.getUserName = async (req, res) => {
 // name: req.names, messages: message
 
 exports.postChats = async (req, res) => {
-  await Chat.create({
-    messages: req.body.chats,
-    userId: req.user,
-  })
-    .then(res.status(201).json())
-    .catch(res.status(401).json());
+  try {
+    const result = await Chat.create({
+      messages: req.body.chats,
+      userId: req.user,
+    });
+    res.status(201).json({
+      message: req.body.chats,
+      name: req.names,
+      id: result.userId,
+      msgId: result.id,
+    });
+  } catch (err) {
+    res.status(401).json();
+  }
 };
--- public/chatPage/chatPage.js
+++ public/chatPage/chatPage.js
@@ -1,8 +1,8 @@
 var token = localStorage.getItem("token");
 document.getElementById("submitChats").addEventListener("click", async () => {
   var inputBox = document.getElementById("userChats");
-  // var chatsFromUser = document.getElementById("userChats").value;
   var chatsFromUser = inputBox.value;
+
   await axios
     .post(
       "http://localhost:4001/chats",
@@ -11,8 +11,10 @@ document.getElementById("submitChats").addEventListener("click", async () => {
       },
       { headers: { Authorization: token } }
     )
-    .then(() => {
-      // chatsFromUser.value = "";
+    .then((result) => {
+      console.log(result.data);
+      saveToLocalStorage(result.data);
+
       inputBox.value = "";
       setTimeout(() => {
         // Scroll to the bottom of the page
@@ -20,22 +22,63 @@ document.getElementById("submitChats").addEventListener("click", async () => {
         lastChild.scrollIntoView({ behavior: "smooth", block: "end" });
       }, 1000);
     })
-    .catch();
+    .catch((err) => console.log(err));
 });
+
+//for saving in localstorage
+function saveToLocalStorage(data) {
+  let myArray = JSON.parse(localStorage.getItem("myArray")) || [];
+
+  let myObj = {
+    userId: data.id,
+    messageId: data.msgId,
+    messages: data.message,
+    user: {
+      name: data.name,
+    },
+  };
+
+  myArray.push(myObj);
+  localStorage.setItem("myArray", JSON.stringify(myArray));
+}
 
 window.addEventListener("DOMContentLoaded", async () => {
   const div = document.getElementById("userMessages");
   // div.innerHTML = ""; // Clear existing messages
-
   setInterval(async () => {
+    var localStorageArray = JSON.parse(localStorage.getItem("myArray")) || 0;
     try {
-      const result = await axios.get("http://localhost:4001/message", {
-        headers: { Authorization: token },
-      });
-      div.innerHTML = ""; // Clear existing messages
-      result.data.messages.forEach((e) => {
-        showMessages(e, e.user.name);
-      });
+      if (localStorageArray.length > 30) {
+        for (let i = 0; i <= 10; i++) {
+          localStorageArray.shift();
+          localStorage.setItem("myArray", JSON.stringify(localStorageArray));
+        }
+      } else if (
+        localStorageArray[0].messageId > 25 &&
+        localStorageArray != 0
+      ) {
+        localStorage.setItem("myArray", JSON.stringify(localStorageArray));
+
+        var messageIdCount = localStorageArray[0].messageId;
+        const result = await axios.get(
+          `http://localhost:4001/message?messageCount=${messageIdCount}`,
+          {
+            headers: { Authorization: token },
+          }
+        );
+
+        var newArrayList = result.data.messages.concat(localStorageArray);
+
+        div.innerHTML = ""; // Clear existing messages
+        newArrayList.forEach((element) => {
+          showMessages(element.messages, element.user.name);
+        });
+      } else {
+        div.innerHTML = ""; // Clear existing messages
+        localStorageArray.forEach((element) => {
+          showMessages(element.messages, element.user.name);
+        });
+      }
     } catch (err) {
       console.log(err);
     }
@@ -48,7 +91,7 @@ function showMessages(data, name) {
   user.id = "spanMessage";
   user.textContent = `${name} : `;
   var span = document.createElement("p");
-  span.textContent = data.messages;
+  span.textContent = data;
   span.id = "poiu";
   user.appendChild(span);
   div.appendChild(user);
